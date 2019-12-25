8d06f2385dea740ceef2d94256702a92746bb50e
Pongchakorn <56287267+Pongchakorn@users.noreply.github.com>
2019-12-25 11:21:34 +0700

    Katalon test

---
+++ Test Suites/ChocoCICD.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>test CICD for azure devops</description>
+   <name>ChocoCICD</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fbe4bc0e-f37e-4cf6-ae99-5d719a0d58b4</testSuiteGuid>
+   <testCaseLink>
+      <guid>0efa1394-adda-42d5-8f7a-46442388f222</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify_text</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
